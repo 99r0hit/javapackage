cca37cff58844e2460219297521e100029a3d4cc
99r0hit <91751811+99r0hit@users.noreply.github.com>
2022-11-03 11:56:31 +0530

    Add files via upload

---
+++ Rohit_TE_42/src/Simple/need_of_division.java
@@ -0,0 +1,15 @@
+package Simple;
+import Rohit_TE_42.division;
+
+public class need_of_division {
+
+	public static void main(String[] args) {
+		
+		division solve = new division();
+		
+		solve.divide();
+		
+
+	}
+
+}
